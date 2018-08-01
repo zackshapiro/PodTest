03e9914b895d4f974b1351e6f4e569c49613b595
Zack Shapiro <zack@zackshapiro.com>
2018-07-31 22:23:30 -0400

    recreated the MBProgressHud failure

---
+++ PodTest/Convo.swift
@@ -0,0 +1,15 @@
+//
+//  Convo.swift
+//  PodTest
+//
+//  Created by Zack Shapiro on 7/31/18.
+//  Copyright © 2018 Zack Shapiro. All rights reserved.
+//
+
+import Foundation
+
+// just need a swift file to mimick the other project
+
+struct Convo {
+    let id: String
+}
